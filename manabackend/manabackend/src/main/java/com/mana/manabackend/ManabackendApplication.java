353504308012069ae710e8b145f3004a5cbf59c6
sitesparkco <sitesparkco@gmail.com>
2024-02-19 10:24:01 +0530

    first commit for implementation

---
+++ manabackend/manabackend/src/main/java/com/mana/manabackend/ManabackendApplication.java
@@ -0,0 +1,13 @@
+package com.mana.manabackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ManabackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ManabackendApplication.class, args);
+	}
+
+}
---
+++ manabackend/manabackend/src/test/java/com/mana/manabackend/ManabackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mana.manabackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ManabackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
